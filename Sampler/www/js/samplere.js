c92d0ac78b01ca9942de42e8f7c46074c65c16f3
Ludovic DEROCHE <ludovic.deroche.thales@gmail.com>
2015-12-08 13:04:05 +0100

    LDER : Began sound search

--- Sampler/www/js/samplere.js
+++ Sampler/www/js/samplere.js
@@ -3,6 +3,29 @@
  */
 'use strict';
 
+var completeSoundList = [];
+
+var accentMap = {
+    "é": "e",
+    "è": "e",
+    "ê": "e",
+    "ë": "e",
+    "à": "a",
+    "â": "a",
+    "ä": "a",
+    "ö": "o",
+    "ô": "o",
+    "ù": "u",
+    "ç": "c"
+};
+var normalize = function( term ) {
+    var ret = "";
+    for ( var i = 0; i < term.length; i++ ) {
+        ret += accentMap[ term.charAt(i) ] || term.charAt(i);
+    }
+    return ret;
+};
+
 function callSound(soundId) {
     $.post('/api/sounds/play/'+soundId, function(data) {
         console.log(data);
@@ -11,14 +34,53 @@ function callSound(soundId) {
 
 function getSounds() {
     $.get('/api/sounds/info', function (data) {
-        console.log('get');
         data.forEach(function(sound) {
-            $('.buttons').append('<a class="btn btn-default col-xs-6 col-sm-3 col-md-2" href="#" role="button" onclick="callSound('+sound.Id+')"><span>'+sound.Name+'</span></a>');
+            $('.buttons').append('<button class="btn btn-default col-xs-6 col-sm-3 col-md-2" href="#" role="button" onclick="callSound('+sound.Id+')"><span>'+sound.Name+'</span></button>');
+            completeSoundList.push({
+                label: sound.Name,
+                value: sound.Id
+            });
         });
 
+        // Autocomplete on search field
+        $('#search').autocomplete({
+            source: function( request, response ) {
+                var matcher = new RegExp( $.ui.autocomplete.escapeRegex( request.term ), "i" );
+                response( $.grep( completeSoundList, function( value ) {
+                    value = value.label || value.value || value;
+                    return matcher.test( value ) || matcher.test( normalize( value ) );
+                }) );
+            },
+            appendTo: '#navbar',
+            delay: 100,
+            select: function(event, ui) {
+                callSound(ui.item.value);
+                $('#search').val(ui.item.label);
+                event.preventDefault();
+            }
+        });
     });
+}
+
+function search($event) {
+    console.log($('#search').val());
+    if($event.keyCode === 13) { // Enter
+        console.log($('#search').autocomplete('option'));
+    } else if($event.keyCode === 27) { // Esc
+        clear();
+    }
+}
+
+function clear() {
+    $('#search').val('');
+}
+
+function submit($event) {
+    console.log($('#search').val());
+    console.log($event);
 }
 
 $().ready(function() {
     getSounds();
+    $('#clearButton span').click(clear);
 });
