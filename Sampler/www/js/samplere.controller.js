65a1bfa1d406fd72124f1615198d11459b88ba50
JP Durot <jeanphilippe.durot@gmail.com>
2015-12-09 12:02:56 +0100

    Merge from ludo pull request

--- Sampler/www/js/samplere.controller.js
+++ Sampler/www/js/samplere.controller.js
@@ -7,6 +7,10 @@ angular.module('samplereApp')
     .controller('MainController',
     function($scope, Sounds) {
 
+
+        // Pour les mobiles, le menu est plié au départ
+        $scope.isCollapsed = true;
+
         var completeSoundList = [];
 
         var accentMap = {
@@ -42,17 +46,6 @@ angular.module('samplereApp')
             $scope.soundList = data;
         });
 
-        function getSounds() {
-            /*$.get('/api/sounds/info', function (data) {
-                data.forEach(function (sound) {
-                    $('.buttons').append('<button class="btn btn-default col-xs-6 col-sm-3 col-md-2" href="#" role="button" onclick="callSound(' + sound.Id + ')"><span>' + sound.Name + '</span></button>');
-                    completeSoundList.push({
-                        label: sound.Name,
-                        value: sound.Id
-                    });
-                });
-            });*/
-        }
 });
 /*
 // Autocomplete on search field
@@ -96,3 +89,55 @@ $().ready(function() {
     $('#clearButton span').click(clear);
 });
 */
+             data.forEach(function (sound) {
+             $('.buttons').append('<button class="btn btn-default col-xs-6 col-sm-3 col-md-2" href="#" role="button" onclick="callSound(' + sound.Id + ')"><span>' + sound.Name + '</span></button>');
+             completeSoundList.push({
+             label: sound.Name,
+             value: sound.Id
+             });
+             });
+             });*/
+        }
+    });
+/*
+ // Autocomplete on search field
+ $('#search').autocomplete({
+ source: function( request, response ) {
+ var matcher = new RegExp( $.ui.autocomplete.escapeRegex( request.term ), "i" );
+ response( $.grep( completeSoundList, function( value ) {
+ value = value.label || value.value || value;
+ return matcher.test( value ) || matcher.test( normalize( value ) );
+ }) );
+ },
+ appendTo: '#navbar',
+ delay: 100,
+ select: function(event, ui) {
+ callSound(ui.item.value);
+ $('#search').val(ui.item.label);
+ event.preventDefault();
+ }
+ });
+
+ function search($event) {
+ console.log($('#search').val());
+ if($event.keyCode === 13) { // Enter
+ console.log($('#search').autocomplete('option'));
+ } else if($event.keyCode === 27) { // Esc
+ clear();
+ }
+ }
+
+ function clear() {
+ $('#search').val('');
+ }
+
+ function submit($event) {
+ console.log($('#search').val());
+ console.log($event);
+ }
+
+ $().ready(function() {
+ getSounds();
+ $('#clearButton span').click(clear);
+ });
+ */
