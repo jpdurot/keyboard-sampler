a9e79dfbb80ceb07d099e9bbcbba6e95cfc5b8f7
NicolasBeaudrot <galip39@gmail.com>
2015-12-15 13:32:47 +0100

    NBE: synchronized mute status

--- Sampler/Server/Controllers/SoundController.cs
+++ Sampler/Server/Controllers/SoundController.cs
@@ -70,6 +70,8 @@ namespace Sampler.Server.Controllers
         {
             _sampler.IsMuted = !_sampler.IsMuted;
 
+            _soundsHubContext.Clients.All.syncIsMuted(_sampler.IsMuted);
+
             return new MuteResponse() { IsMuted = _sampler.IsMuted };
         }
 
