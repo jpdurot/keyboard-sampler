86c85be6bca83395f387959d6e81f024061b0b8b
Ludovic DEROCHE <ludovic.deroche.thales@gmail.com>
2015-12-18 22:45:48 +0100

    LDER : Fixes

--- Sampler/www/js/samplere.config.js
+++ Sampler/www/js/samplere.config.js
@@ -34,7 +34,7 @@ angular.module('samplereApp').config(
       });
 
       // Redirects users not logged in to login page on route changes
-      var checkLoggedin = function($q, $timeout, Login, $location, $rootScope) {
+      var checkLoggedin = function($q, $timeout, Login, $location, $rootScope,$localStorage) {
         // Initialize a new promise
         var deferred = $q.defer();
 
@@ -72,10 +72,33 @@ angular.module('samplereApp').config(
 
         return deferred.promise;
       };
+      
+      var checkNotLoggedIn = function($localStorage, $location, $timeout, $q) {
+        console.log('checkNotLoggedIn')
+        // Initialize a new promise
+        var deferred = $q.defer();
+        // Needs a timeout to force a digest cycle, else the url is not
+        // changed
+        if(angular.isDefined($localStorage.token)) {
+          console.log('already logged in')
+          deferred.reject();
+          $timeout(function() {
+            $location.url('/');
+          }, 0);
+        } else {
+          deferred.resolve();
+        }
+        return deferred.promise;
+      }
 
       $routeProvider.when('/login', {
         controller : 'LoginController',
-        templateUrl : 'views/login/login.html'
+        templateUrl : 'views/login/login.html',
+        resolve : {
+          notLoggedIn : function($localStorage, $location, $timeout, $q) {
+            return checkNotLoggedIn($localStorage, $location, $timeout, $q);
+          }
+        }
       }).when('/change_password', {
         controller : 'ChangePasswordController',
         templateUrl : 'views/change_password/change_password.html'
@@ -83,16 +106,16 @@ angular.module('samplereApp').config(
         controller : 'SoundsController',
         templateUrl : 'views/sounds/sounds.html',
         resolve : {
-          loggedIn : function() {
-            return checkLoggedin;
+          loggedIn : function($q, $timeout, Login, $location, $rootScope,$localStorage) {
+            return checkLoggedin($q, $timeout, Login, $location, $rootScope,$localStorage);
           }
         }
       }).when('/favorites', {
         controller : 'SoundsController',
         templateUrl : 'views/sounds/favorites.html',
         resolve : {
-          loggedIn : function() {
-            return checkLoggedin;
+          loggedIn : function($q, $timeout, Login, $location, $rootScope,$localStorage) {
+            return checkLoggedin($q, $timeout, Login, $location, $rootScope,$localStorage);
           }
         }
       }).otherwise({
