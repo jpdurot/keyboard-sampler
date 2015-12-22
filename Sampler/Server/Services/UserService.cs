e9bf56c4eb7ad2f7d2e01d3612c0d087f2bf675a
cyrildegout <cyril.degout@gmail.com>
2015-12-22 18:09:23 +0100

    [CDT] Add new util file and UserService

---
+++ Sampler/Server/Services/UserService.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Sampler.Server.Controllers;
+using Sampler.Server.Model;
+using Sampler.Server.Utils;
+
+namespace Sampler.Server.Services
+{
+    public class UserService
+    {
+        #region Singleton
+        private static UserService _instance;
+
+        public static UserService Current
+        {
+            get
+            {
+                return (_instance ?? (_instance = new UserService()));
+            }
+        }
+        #endregion
+
+        private const int ChatMaxHistory = 10;
+
+        private readonly List<ChatMessage> _chatHistory;
+        public List<ChatMessage> ChatHistory
+        {
+            get { return _chatHistory; }
+        }
+
+        public UserService()
+        {
+            _chatHistory = new List<ChatMessage>();
+        }
+
+        /// <summary>
+        /// Gets the user by id
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public User GetUser(int userId)
+        {
+            return DataBaseService.Current.Db.Table<User>()
+                .FirstOrDefault(
+                    u =>
+                        u.Id == userId);
+        }
+
+        /// <summary>
+        /// Add a sound to the user list
+        /// </summary>
+        /// <param name="soundId">the sound id</param>
+        /// <param name="user">the user</param>
+        /// <returns>true if successfully updated, false instead</returns>
+        public bool AddPlayedSound(User user, int soundId)
+        {
+            user.PlaySoundCount++;
+            return DataBaseService.Current.Db.Update(user) > 0;
+        }
+
+        /// <summary>
+        /// Modify the user password
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="oldPassword"></param>
+        /// <param name="newPassword"></param>
+        public bool ModifyPassword(User user, string oldPassword, string newPassword)
+        {
+            if (string.Equals(user.Password, Encryption.HashMd5(oldPassword), StringComparison.OrdinalIgnoreCase))
+            {
+                user.Password = Encryption.HashMd5(newPassword);
+                return DataBaseService.Current.Db.Update(user) > 0;
+            }
+            return false;
+        }
+
+        /// <summary>
+        ///  Store previous chat in memory
+        /// </summary>
+        /// <param name="name">user name</param>
+        /// <param name="message">chat message</param>
+        public ChatMessage AddChatMessage(string name, string message)
+        {
+            if (_chatHistory.Count >= ChatMaxHistory)
+            {
+                _chatHistory.RemoveAt(0);
+            }
+
+            var chatMessage = new ChatMessage()
+            {
+                Name = name,
+                Message = message,
+                Time = DateTime.Now.ToString("dd/MM à HH:mm")
+            };
+            _chatHistory.Add(chatMessage);
+
+            return chatMessage;
+        }
+
+        /// <summary>
+        /// Return all users from database
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<User> GetAllUsers()
+        {
+            return DataBaseService.Current.Db.Table<User>().ToList();
+        }
+    }
+}
---
+++ Sampler/Server/Utils/Encryption.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sampler.Server.Utils
+{
+    public class Encryption
+    {
+        internal static string HashMd5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            MD5 md5 = MD5.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+            // Convert the byte array to hexadecimal string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                sb.Append(hashBytes[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
